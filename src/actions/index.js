ec4a815fdb3d8b04941df992bdc49508c762bfe6
Lavi <lavisharma796@gmail.com>
2019-01-25 06:17:30 +0000

    replace the BrowserRoter with Router for programmatic navigation create the history object ourselves

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import {
     SIGN_IN,
     SIGN_OUT,
@@ -25,13 +26,14 @@ export const signOut = () => {
 
 export const createStream = formValues => async(dispatch, getState) => {
     const { userId } = getState().auth;
-
     const response = await streams.post('/streams', { ...formValues, userId });
 
     dispatch({
         type: CREATE_STREAM,
         payload: response.data
     });
+    // programmatic navigation
+    history.push('/');
 };
 
 export const fetchStreams = () => async dispatch => {
---
+++ src/history.js
@@ -0,0 +1,4 @@
+import createHistory from 'history/createBrowserHistory';
+// history package was installed by the 'react-router-dom'
+
+export default createHistory();
